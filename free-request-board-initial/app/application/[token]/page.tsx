811bc1fde87a3f114036d24ca10ec5ebf779952e
臼井事務所 <takanashihiroyama@gmail.com>
2026-05-28 07:53:53 +0900

    Update page.tsx

--- free-request-board-initial/app/application/[token]/page.tsx
+++ free-request-board-initial/app/application/[token]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import { supabaseAdmin } from "@/lib/supabaseAdmin";
+import { supabaseAdmin } from "../../../lib/supabaseAdmin";
 
 export const dynamic = "force-dynamic";
 
