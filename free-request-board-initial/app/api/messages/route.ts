50ac80c5c166cb509484a157e67479577cab2fed
臼井事務所 <takanashihiroyama@gmail.com>
2026-05-28 07:43:53 +0900

    Update route.ts

--- free-request-board-initial/app/api/messages/route.ts
+++ free-request-board-initial/app/api/messages/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { redirect } from "next/navigation";
-import { supabaseAdmin } from "@/lib/supabaseAdmin";
+import { supabaseAdmin } from "../../../lib/supabaseAdmin";
 
 export async function POST(request: Request) {
   const form = await request.formData();
